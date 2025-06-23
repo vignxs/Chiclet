bba383d88d3050084c09b254144510bac2b7b56f
vigneshax <vsivakumar@affinityx.com>
2025-06-24 01:23:30 +0530

    image

--- chiclet/next.config.ts
+++ chiclet/next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'i.pinimg.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'hfvpccmnuvbbusosrnhc.supabase.co' },
     ],
   },
 }
